495394a078b18f7b1fe3d6fed0e9dbce30fdaa1f
Ilyas Abdighni <udunig@gmail.com>
2017-10-16 07:57:00 +0200

    graphql firstly set up

--- schema/schema.js
+++ schema/schema.js
@@ -6,19 +6,34 @@
 
 const graphql = require('graphql');
 const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = graphql;
-const _ = require('lodash');
+const axios = require('axios');
 
 const users = [
   { id: '23', firstName: 'Ilyas', age: 20 },
-  { id: '47', firstName: 'Ilyas', age: 21 }
+  { id: '47', firstName: 'Abdighni', age: 21 }
 ];
+
+const CompanyType = new GraphQLObjectType({
+  name: 'Company',
+  fields: {
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    description: { type: GraphQLString }
+  }
+});
 
 const UserType = new GraphQLObjectType({
   name: 'User',
   fields: {
     id: { type: GraphQLString },
     firstName: { type: GraphQLString },
-    age: { type: GraphQLInt }
+    age: { type: GraphQLInt },
+    company: {
+      type: CompanyType,
+      resolve(parentValue, args) {
+        console.log(parentValue, args);
+      }
+    }
   }
 });
 
@@ -29,7 +44,9 @@ const RootQuery = new GraphQLObjectType({
       type: UserType,
       args: { id: { type: GraphQLString } },
       resolve(parentValue, args) {
-        return _.find(users, { id: args.id });
+        return axios
+          .get(`http://localhost:3000/users/${args.id}`)
+          .then(res => res.data);
       }
     }
   }
